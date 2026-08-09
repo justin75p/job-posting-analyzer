47a82be4bfff340cd81ff1c1a3fbbb03900bbfc7
Justin Phan <justin75p@gmail.com>
2026-08-08 20:06:34 -0700

    move KNOWN_SKILLS to a new file skills.py for shared use

---
+++ src/skills.py
@@ -0,0 +1,37 @@
+KNOWN_SKILLS = [
+    # Programming Languages
+    "Python", "Java", "JavaScript", "TypeScript", "C++", "C#", "R", "Go", "Rust", "Swift", "Kotlin", "SQL",
+
+    # Web Frameworks
+    "Flask", "Django", "FastAPI", "React", "Vue", "Angular", "Express", "Spring", "Next.js", "Node.js",
+
+    # Databases
+    "PostgreSQL", "MySQL", "MongoDB", "SQLite", "Redis", "Elasticsearch", "DynamoDB", "Cassandra",
+
+    # Cloud
+    "AWS", "Azure", "GCP", "Lambda", "S3", "EC2", "RDS", "CloudFormation", "Terraform", "Docker", "Kubernetes",
+
+    # ML / Data Science
+    "scikit-learn", "TensorFlow", "PyTorch", "Keras", "pandas", "NumPy", "spaCy", "NLTK", "Hugging Face", "XGBoost",
+
+    # Tools
+    "Git", "GitHub", "Linux", "REST APIs", "GraphQL", "CI/CD", "Jenkins", "Jira", "Postman",
+
+    # Data / Analytics
+    "Tableau", "Power BI", "Spark", "Hadoop", "Airflow", "dbt", "Snowflake", "BigQuery",
+
+    # APIs / Protocols
+    "JSON", "XML", "gRPC", "WebSocket", "OAuth",
+
+    # Mobile
+    "React Native", "Flutter", "Android", "iOS",
+
+    # Testing
+    "pytest", "JUnit", "Selenium", "Jest",
+
+    # Concepts
+    "machine learning", "deep learning", "NLP", "computer vision", "data analysis", "agile", "object-oriented programming",
+
+    # Other
+    "Streamlit", "Jupyter", "MATLAB", "Bash", "PowerShell", "Excel"
+]
